3d06f58e673d36fdec6cbfbeaca0236a5b113b8e
Prashant <nishadprashant@outlook.com>
2022-07-11 15:04:30 +0530

    second commit

---
+++ MVCWepApp/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWepApp.Models
+{
+    public class Employee
+    {
+        public int EId { get; set; }
+        public string EName { get; set; }
+        public string EDesignation { get; set; }
+        public DateTime EDOJ { get; set; }
+    }
+}
---
+++ MVCWepApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCWepApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
